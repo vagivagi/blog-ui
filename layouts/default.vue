0e042b36e66284208673856c089153c0adcb3550
vagivagi <hagikazu7@gmail.com>
2020-03-14 19:49:15 +0900

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,17 @@
 <template>
   <v-app>
     <nav>
-    <v-toolbar>
-      <v-toolbar-title class="pr-4">
-        <router-link :to="to" class="toolbar-title" tag="h3">{{ title }}</router-link>
-      </v-toolbar-title>
-      <v-toolbar-items>
-      <v-btn
-        v-for="(item, i) in items"
-        :key="i"
-        :to="item.to"
-        text>
-        <v-icon left>{{ item.icon }}</v-icon>
-        <div>{{ item.title }}</div>
-      </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
+      <v-toolbar>
+        <v-toolbar-title class="pr-4">
+          <router-link :to="to" class="toolbar-title" tag="h3">{{ title }}</router-link>
+        </v-toolbar-title>
+        <v-toolbar-items>
+          <v-btn v-for="(item, i) in items" :key="i" :to="item.to" text>
+            <v-icon left>{{ item.icon }}</v-icon>
+            <div>{{ item.title }}</div>
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
     </nav>
     <v-content>
       <v-container>
@@ -23,20 +19,16 @@
       </v-container>
     </v-content>
     <Cta />
-    <v-footer
-      :fixed="fixed"
-      app
-      text-xs-center
-    >
+    <v-footer :fixed="fixed" app text-xs-center>
       <v-flex text-xs-center>
-        <span>&copy; 2019</span>
+        <span>&copy; {{ year.getFullYear() }}</span>
       </v-flex>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-import Cta from '~/components/Cta.vue'
+import Cta from "~/components/Cta.vue";
 
 export default {
   components: {
@@ -48,31 +40,32 @@ export default {
       drawer: false,
       fixed: false,
       show: false,
+      year: new Date(),
       items: [
         {
-          icon: 'description',
-          title: '記事一覧',
-          to: '/entries'
+          icon: "description",
+          title: "記事一覧",
+          to: "/entries"
         },
         {
-          icon: 'layers',
-          title: 'カテゴリ',
-          to: '/categories'
+          icon: "layers",
+          title: "カテゴリ",
+          to: "/categories"
         },
         {
-          icon: 'label',
-          title: 'タグ',
-          to: '/tags'
+          icon: "label",
+          title: "タグ",
+          to: "/tags"
         }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'vagivagi blog',
-      to: '/'
-    }
+      title: "vagivagi blog",
+      to: "/"
+    };
   }
-}
+};
 </script>
 <style>
 .toolbar-title {
