d22cdfcc017d21de14d8783b748c989f45a89b39
vagivagi <hagikazu7@gmail.com>
2021-03-14 12:26:46 +0900

    Update index.vue

--- pages/english/index.vue
+++ pages/english/index.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="main-content">
     <EnglishStudyNav />
-    <h4 class="headline">概要</h4>
-    <v-divider/>
-    <br>
-    <p class="text-justify">
-      英語の勉強方法や記録をまとめるページです。<br>
-      どんな方法で勉強しているか、どれくらい勉強したかをまとめる予定です。
-    </p>
+    <v-container fluid>
+    <v-row>
+      <v-col>
+        <div class="profile" v-html="$md.render(index)" />
+      </v-col>
+    </v-row>
+  </v-container>
   </div>
 </template>
 
 <script>
 import EnglishStudyNav from "~/components/EnglishStudyNav.vue";
+import axios from "axios";
 
 export default {
   components: {
@@ -22,6 +23,25 @@ export default {
     return {
       title: `コロナ禍なので、英語の勉強を頑張ってみた`
     };
+  },
+  data: function() {
+    return {
+      id: this.githubId,
+      index: "Now Rendering"
+    };
+  },
+  mounted () {
+    const path =
+      "https://api.github.com/repos/vagivagi/english-post/contents/index.md";
+    const config = {
+      headers: {
+        'Accept': 'application/vnd.github.v3.raw',
+        'Authorization' : 'token ' + process.env.GITHUB_ACCESS_TOKEN
+      }
+    };
+    axios
+      .get(path, config)
+      .then(response => (this.index = response.data));
   }
 };
 </script>
