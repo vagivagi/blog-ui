d8c22a004c7958c7357253f8292e8fbb360ea6be
vagivagi <hagikazu7@gmail.com>
2025-05-06 10:16:50 +0900

    Fix for LatestEntries

--- components/LatestEntries.vue
+++ components/LatestEntries.vue
@@ -6,19 +6,12 @@
     </ul>
 </template>
 <script lang="ts">
-import axios from 'axios'
-
 export default {
-  data() {
-    return {
-      entries: [] // 初期値を空配列に設定
-    };
-  },
-  async created() {
-    const baseUrl = process.env.API_BASE_URL || 'http://localhost:8080';
-    const api = `${baseUrl}/entries`;
-    const res = await axios.get(api);
-    this.entries = res.data.slice(0, 5);
+  props: {
+    entries: {
+      type: Array,
+      required: true // 親コンポーネントからデータを必須で受け取る
+    }
   }
-}
+};
 </script>
